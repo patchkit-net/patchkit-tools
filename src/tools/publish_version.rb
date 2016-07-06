faf565dece12ad4453f2aab967d9c7a364810478
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-07-06 10:44:40 +0200

    Added version publishing

--- src/tools/publish_version.rb
+++ src/tools/publish_version.rb
@@ -15,9 +15,21 @@ options.parse(__FILE__ != $0 ? $passed_args : ARGV) do |opts|
     "user API key") do |api_key|
     options.api_key = api_key
   end
+
+  opts.on("-v", "--version VERSION", Integer,
+    "version to publish") do |version|
+    options.version = version
+  end
 end
 
 options.error_argument_missing("secret") if options.secret.nil?
 options.error_argument_missing("apikey") if options.api_key.nil?
+options.error_argument_missing("version") if options.version.nil?
 
-raise "Not implemented"
+resource_name = "1/apps/#{options.secret}/versions/#{options.version}/publish?api_key=#{options.api_key}"
+
+puts "Publishing veresion..."
+PatchKitAPI::ResourceRequest.new(resource_name, nil, Net::HTTP::Put).get_object do |object|
+  puts "Result: #{object}"
+  puts "Done!"
+end
