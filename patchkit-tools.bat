075294251b096d08659fb0b1bf95e007eda4c407
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-09-03 18:40:53 +0200

    Improved Windows starter script

--- patchkit-tools.bat
+++ patchkit-tools.bat
@@ -1,15 +1,41 @@
 @echo off
 
-set PROGRAM_ARGS=
-set PROGRAM_NAME=%1
-set PROGRAM_NAME="%~dp0/src/%PROGRAM_NAME:-=_%.rb"
-shift
+setlocal EnableDelayedExpansion
 
-:loop
-if [%1]==[] goto after_loop
-set PROGRAM_ARGS=%PROGRAM_ARGS% %1
-shift
-goto loop
+set TOOL=
+set TOOL_NAME=%1
 
-:after_loop
-ruby %PROGRAM_NAME%%PROGRAM_ARGS%
+if not [%1] == [] (
+  if not "%1:~0,1%" == "-" (
+    set TOOL="%~dp0/src/%TOOL_NAME:-=_%.rb"
+  )
+)
+
+if not [%TOOL%] == [] (
+  if exist "%TOOL%" (
+    if not "%1" == "--help" (
+      if not "%1" == "-h" (
+        set TOOL_ARGS=
+
+        :loop
+        if "%1"=="" goto after_loop
+        set TOOL_ARGS=%TOOL_ARGS% %1
+        shift
+        goto loop
+
+        :after_loop
+        ruby %TOOL%%TOOL_ARGS%
+        exit /b ERRORLEVEL
+      )
+    )
+  )
+)
+
+echo Usage: patchkit-tools [tool_name] [tool_arguments]
+echo.
+echo Available tools:
+for  %%i in ("%~dp0\src\*.rb") do (
+  set z=%%~ni
+  echo     !z:_=-!
+)
+exit /b 1
