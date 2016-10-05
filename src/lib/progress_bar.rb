6fe84f5d3206aefe2b9931495594b498494c20ba
Tomasz Jaworski <wiedzminek112@gmail.com>
2016-10-05 15:44:19 +0200

    Fixed progress bar displaying

--- src/lib/progress_bar.rb
+++ src/lib/progress_bar.rb
@@ -13,18 +13,19 @@ class ProgressBar
       if @semaphore.try_lock
         console_width = IO.console.winsize[1]
         if @lines_taken > 0
-          $stdout.syswrite "\e[1A\r"
-          $stdout.syswrite " " * @previous_status_length if @previous_status_length > status.length
-          $stdout.syswrite "\e[#{@lines_taken - 1}A\r"
+          $stdout.write "\e[1A\r"
+          $stdout.write " " * @previous_status_length if @previous_status_length > status.length
+          $stdout.write "\e[#{@lines_taken - 1}A\r"
+          $stdout.flush
         end
 
         progress_bar_length = console_width - 5
         progress_length = (progress_bar_length.to_f * get_progress_value(progress)).round
         if(progress_length == progress_bar_length)
-          $stdout.syswrite "|#{'=' * progress_length}|\n#{status}\n"
+          $stdout.write "|#{'=' * progress_length}|\n#{status}\n"
         else
           remaining_length = progress_bar_length - progress_length
-          $stdout.syswrite "|#{'=' * [0, (progress_length - 1)].max}#{'>' * [1, (progress_length)].min}#{'-' * remaining_length}|\n#{status}\n"
+          $stdout.write "|#{'=' * [0, (progress_length - 1)].max}#{'>' * [1, (progress_length)].min}#{'-' * remaining_length}|\n#{status}\n"
         end
         $stdout.flush
         @lines_taken = (status.length.to_f / console_width.to_f).ceil + 1
