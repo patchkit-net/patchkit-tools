ca9647932bf81bcc3106ae5f20177dbca20ab620
Tomasz Jaworski <tomasz.jaworski98@gmail.com>
2017-09-05 18:20:15 +0200

    Fix Windows starting shell scripts

--- patchkit-tools.bat
+++ patchkit-tools.bat
@@ -1,7 +1,5 @@
 @echo off
 
-@echo off
-
 :: http://stackoverflow.com/questions/2952401/remove-trailing-slash-from-batch-file-input
 set current_dir=%~dp0
 IF %current_dir:~-1%==\ SET current_dir=%current_dir:~0,-1%
