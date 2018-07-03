d5bf30642d9134ea2d0aa7347a94bf0b7151138d
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2018-07-03 09:44:34 +0200

    Grammar fix

--- app/create_version.rb
+++ app/create_version.rb
@@ -67,7 +67,7 @@ module PatchKitTools
       result = PatchKitAPI.post(request_path, params: { label: self.label })
 
       @created_version_id = result[:id]
-      puts "A new version of id #{@created_version_id} has been created!"
+      puts "A new version with id #{@created_version_id} has been created!"
     end
   end
 end
