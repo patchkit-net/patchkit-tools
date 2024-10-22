7eed5c769d6dfe94dd7f9ac22d649504a2f55948
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2024-10-22 09:00:40 +0200

    Signatures downloading: fix download for files bigger than 512
    It was using the wrong download method if the signatures file has been hosted with S3 signed URL instead of CDN url.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/) 
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## [UNRELEASED]
+### Fixed
+- Signatures downloading could fail in come cases when the signatures file was larger than 512 megabytes
+
 ## [3.6.0]
 ### Added
 - TurboPatch Support
--- app/core/model/version.rb
+++ app/core/model/version.rb
@@ -71,6 +71,14 @@ module PatchKitTools
           return download_signatures_fallback(offset: 0, &block)
         end
 
+        # That could be done better.
+        # When a link is served as a presigned URL (that starts with s3.REGION), the signautres are expected
+        # to be a complete regular file, therefore no fancy download here
+        if url.include? '//s3.'
+          puts "Got signed url, downloading..."
+          return download_signatures_regular(url: url, size: size, offset: offset, &block)
+        end
+
         part_size = 1024**2 * 512
         parts = size / part_size
         parts += 1 if size % part_size != 0
@@ -102,6 +110,18 @@ module PatchKitTools
         request = PatchKitAPI::ResourceRequest.new(path)
         request.offset = offset
         request.get_response { |r| yield r }
+      end
+
+      def download_signatures_regular(url:, size:, offset: 0)
+        uri = URI(url)
+        Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new uri
+          request['Range'] = "bytes=#{offset}-" if offset > 0
+
+          http.request request do |response|
+            yield response, size
+          end
+        end
       end
     end
   end
